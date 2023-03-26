90a723df0ae674ba24b46ce398a26b6846fbe82e
Manuel <manuel.torrado@gmail.com>
2023-03-26 03:56:47 -0300

    Startup

---
+++ init.vim
@@ -0,0 +1,8 @@
+lua require('plugins') 
+colorscheme tokyonight
+set number
+set mouse=a
+syntax on
+lua require('nvim-tree').setup()
+:NvimTreeToggle
+
---
+++ lua/plugins.lua
@@ -0,0 +1,15 @@
+return require('packer').startup(function(use)
+use 'wbthomason/packer.nvim'
+
+
+--Tab manager
+use 'nvim-tree/nvim-web-devicons'
+use {'nvim-tree/nvim-tree.lua'}
+use {'romgrk/barbar.nvim', requires = 'nvim-web-devicons'}
+-- Tokyonight theme
+use 'folke/tokyonight.nvim'
+
+--File Tree
+
+end)
+
